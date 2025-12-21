008cb001ab9f358583f962c4d1f0dbf183cd1d26
edsonbastos2 <edsonbastos2@hotmail.com>
2025-12-21 12:07:49 -0300

    feat:adiciona dto, metodo de lista um produto

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDto.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDto() {
+    }
+
+    public ProductDto(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDto(Product product) {
+        id = product.getId();
+        name = product.getName();
+        description = product.getDescription();
+        price = product.getPrice();
+        imgUrl = product.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
